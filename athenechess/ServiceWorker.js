a3c55b2a8b4181f961bd09c77706414114833def
bach <cxbach@gmail.com>
2024-07-24 14:43:25 +0700

    - Fix Get

--- athenechess/ServiceWorker.js
+++ athenechess/ServiceWorker.js
@@ -5,29 +5,33 @@ const contentToCache = [
     "Build/athenechess.data",
     "Build/athenechess.wasm",
     "TemplateData/style.css"
-
 ];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
-    
+
     e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
+        const cache = await caches.open(cacheName);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(contentToCache);
     })());
 });
 
 self.addEventListener('fetch', function (e) {
     e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
+        // Chỉ xử lý các yêu cầu GET
+        if (e.request.method !== 'GET') {
+            return fetch(e.request);
+        }
 
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
+        let response = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (response) { return response; }
+
+        response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
     })());
 });
